d841c6ac05ad9f32631bbd17a4b00780ba9f675e
CheryX <roxmb25@gmail.com>
2022-05-14 23:04:03 +0200

    Final Polishes

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import { textBox } from './lib/textBox.js';
-import { wrapText } from './lib/wrapText.js';
-export { textBox, wrapText };
+import { wrapText, getLines } from './lib/wrapText.js';
+export { textBox, wrapText, getLines };
